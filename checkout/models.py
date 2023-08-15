c51614ef0970519597acc04e0ff2fd404361d371
Amr Azouz <amrsalahazouz@gmail.com>
2023-08-15 14:01:33 +0300

    #

--- checkout/models.py
+++ checkout/models.py
@@ -5,6 +5,7 @@ class CashoutRequest(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     requested_points = models.IntegerField(default=0)  # Representing the requested points
     payment_method = models.CharField(max_length=255)
+    payment_address = models.CharField(max_length=255, blank=True)
     status = models.CharField(max_length=255)
 
     def __str__(self):
--- checkout/serializers.py
+++ checkout/serializers.py
@@ -4,4 +4,4 @@ from .models import CashoutRequest
 class CashoutRequestSerializer(serializers.ModelSerializer):
     class Meta:
         model = CashoutRequest
-        fields = ('id', 'user', 'requested_points', 'payment_method', 'status')
+        fields = ('id', 'user', 'requested_points', 'payment_method','payment_address', 'status')
